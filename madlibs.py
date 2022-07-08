acfae63343973f135d087e0b548eb7f17b2030dd
Ahmad Abuzaina <ah.abuzaina@hotmail.com>
2022-07-08 16:35:18 -0700

    first commit

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+# youtuber = ""
+# print(f"subrscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("verb: ")
+verb2 = input("verb: ")
+
+madlib = f"Ahmad is really {adj}! something about him {verb1} for the sky. somethinh tells me he {verb2} everyday"
+
+print(madlib)
---
+++ print("hello world").py
@@ -0,0 +1,5 @@
+def printHelloWorld():
+    print("hello world")
+
+x = printHelloWorld()
+print("aaaa")
