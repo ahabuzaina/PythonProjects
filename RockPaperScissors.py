e2d44d5fddabf0b4a78b4019eb49b79a9f8097b5
Ahmad Abuzaina <ah.abuzaina@hotmail.com>
2022-07-11 20:35:22 -0700

    rockpaperscissorsupdate

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -1,19 +1,21 @@
 import random
 
+from regex import R
+
 def play():
     user = input("choose 'r' for rock, 'p' for paper, 's' for scissors")
     computer = random.choice(["r","p","s"])
     print(f"computer has chosen {computer}")
 
     if user == computer:
-        print("tie")
+        return "Tie"
     elif user == "r" and computer == "s":
-        print("you win")
+        return "you win"
     elif user == "p" and computer == "r":
-        print("you win")
+        return "you win"
     elif user == "s" and computer == "p":
-        print("you win")
-    else:
-        print("you lose")
+        return "you win"
+    
+    return "you lost"
 
-play()
+print(play())
